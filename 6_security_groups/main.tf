283ef179a4a8346d3eff9c33974a1ba10974f6da
Mahboob <mahboobmonnamd@gmail.com>
2024-02-18 10:02:45 +0530

    tf security groups

---
+++ 6_security_groups/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "terraform-ec2" {
+  ami             = "ami-0c20d88b0021158c6" # take it from console
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name]
+}
+
+resource "aws_security_group" "webtraffic" {
+  name = "Allow HTTPS"
+
+  ingress = [{
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
+
+  egress = [{
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
+}
